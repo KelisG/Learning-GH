b560b8b6e71d74c8236ca9e43760530a0e199d8d
Kelis Gipson <Kelisjgio>
2021-05-22 18:29:17 -0700

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Kelis Gipson on 5/22/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
